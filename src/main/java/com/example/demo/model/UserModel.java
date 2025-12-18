e2987f3e5f7e9aba536710c651126af60bd01227
bala-amypo <bala@amypo.in>
2025-12-18 05:11:44 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/UserModel.java
@@ -0,0 +1,82 @@
+package com.example.demon.model;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.*;
+
+@Entity
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false)
+    private String fullName;
+
+    @Column(unique=true,nullable=false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable=false)
+    private String role;
+    
+    private String preferredLearningStyle;
+    private LocalDateTime createdAt;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public String getPreferredLearningStyle() {
+        return preferredLearningStyle;
+    }
+    public void setPreferredLearningStyle(String preferredLearningStyle) {
+        this.preferredLearningStyle = preferredLearningStyle;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public UserModel(Long id, String fullName, String email, String password, String role,
+            String preferredLearningStyle, LocalDateTime createdAt) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.preferredLearningStyle = preferredLearningStyle;
+        this.createdAt = createdAt;
+    }
+    public UserModel() {
+    }
+
+    
+}
