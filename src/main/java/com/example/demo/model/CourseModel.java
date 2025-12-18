36692f156d33afcb9d99afda4c7d915867d335c6
bala-amypo <bala@amypo.in>
2025-12-18 06:03:06 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/CourseModel.java
@@ -0,0 +1,68 @@
+package com.example.demo.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class CourseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private UserModel instructor;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public UserModel getInstructor() {
+        return instructor;
+    }
+    public void setInstructor(UserModel instructor) {
+        this.instructor = instructor;
+    }
+    public String getCategory() {
+        return Category;
+    }
+    public void setCategory(String category) {
+        Category = category;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    private String Category;
+    private LocalDateTime createdAt;
+    public CourseModel(Long id, String title, String description, UserModel instructor, String category,
+            LocalDateTime createdAt) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.instructor = instructor;
+        Category = category;
+        this.createdAt = createdAt;
+    }
+    public CourseModel() {
+    }
+    
+}
