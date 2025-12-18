489353a7ca8578a98f6269ee4c1c1e97d511a55e
bala-amypo <bala@amypo.in>
2025-12-18 08:02:42 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/MicroLessonModel.java
@@ -0,0 +1,112 @@
+package com.example.demo.model;
+
+import java.time.LocalDate;
+import jakarta.persistence.*;
+
+@Entity
+public class MicroLesson {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private Integer durationMinutes;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ContentType contentType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Difficulty difficulty;
+
+    private String tags;
+
+    private LocalDate publishDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getDurationMinutes() {
+        return durationMinutes;
+    }
+
+    public void setDurationMinutes(Integer durationMinutes) {
+        this.durationMinutes = durationMinutes;
+    }
+
+    public ContentType getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(ContentType contentType) {
+        this.contentType = contentType;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public LocalDate getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(LocalDate publishDate) {
+        this.publishDate = publishDate;
+    }
+    public MicroLesson() {
+    }
+
+    public MicroLesson(Course course, String title, Integer durationMinutes,
+                       ContentType contentType, Difficulty difficulty,
+                       String tags, LocalDate publishDate) {
+        this.course = course;
+        this.title = title;
+        this.durationMinutes = durationMinutes;
+        this.contentType = contentType;
+        this.difficulty = difficulty;
+        this.tags = tags;
+        this.publishDate = publishDate;
+    }
+}
