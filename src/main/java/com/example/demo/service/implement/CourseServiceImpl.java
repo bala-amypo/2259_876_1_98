c75a1ed1eed11adff46b688affdc32df7d452280
bala-amypo <bala@amypo.in>
2025-12-19 03:30:37 +0000

    User commit

--- src/main/java/com/example/demo/service/implement/CourseServiceImpl.java
+++ src/main/java/com/example/demo/service/implement/CourseServiceImpl.java
@@ -4,8 +4,9 @@ import com.example.demo.model.Course;
 import com.example.demo.repository.CourseRepository;
 import com.example.demo.service.CourseService;
 
+@Service
 public class CourseServiceImpl implements CourseService {
-    private CourseRepository courseRepository;
+
     @Override
     public Course create(Course course){
         return courseRepository.save(course);
