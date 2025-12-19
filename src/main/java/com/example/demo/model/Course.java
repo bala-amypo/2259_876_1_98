a2e825dec1f1e52d614f73b49ed9fc44df21456e
bala-amypo <bala@amypo.in>
2025-12-19 05:10:17 +0000

    User commit

--- src/main/java/com/example/demo/model/Course.java
+++ src/main/java/com/example/demo/model/Course.java
@@ -1,10 +1,11 @@
-package com.example.demo.model;
-
-import jakarta.persistence.*;
-import lombok.*;
+package com.example.demo.entity;
 
 import java.time.LocalDateTime;
-import java.util.List;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
 @Entity
 @Getter
@@ -12,26 +13,31 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name = "courses")
 public class Course {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @Size(max = 150)
     @Column(nullable = false)
     private String title;
 
+    @Size(max = 500)
+    @Column(nullable = true)
     private String description;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "instructor_id", nullable = false)
     private User instructor;
 
+    @NotBlank
+    @Size(max = 50)
+    @Column(nullable = false)
     private String category;
 
     private LocalDateTime createdAt;
-
 
     @PrePersist
     public void onCreate() {
