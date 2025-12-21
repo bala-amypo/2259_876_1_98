6fb01501e7bc437a27754dfdb2e590b17d24458e
bala-amypo <bala@amypo.in>
2025-12-21 13:56:47 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/RecommendationServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RecommendationServiceImpl.java
@@ -1,41 +1,37 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.dto.RecommendationRequest;
-import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Recommendation;
-import com.example.demo.model.User;
 import com.example.demo.repository.RecommendationRepository;
-import com.example.demo.repository.UserRepository;
 import com.example.demo.service.RecommendationService;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class RecommendationServiceImpl implements RecommendationService {
 
-    private final RecommendationRepository recommendationRepository;
+    private final RecommendationRepository repo;
 
-    public RecommendationServiceImpl(RecommendationRepository recommendationRepository) {
-        this.recommendationRepository = recommendationRepository;
+    public RecommendationServiceImpl(RecommendationRepository repo) {
+        this.repo = repo;
     }
 
     @Override
     public Recommendation generateRecommendation(RecommendationRequest request) {
-        Recommendation recommendation = new Recommendation();
-        recommendation.setRecommendedLessonIds("1,2,3");
-        recommendation.setBasisSnapshot("demo");
-        recommendation.setConfidenceScore(BigDecimal.valueOf(0.9));
 
+        Recommendation recommendation = Recommendation.builder()
+                .recommendedLessonIds("1,2,3")
+                .basisSnapshot("demo")
+                .confidenceScore(BigDecimal.valueOf(0.9))
+                .build();
 
-        return recommendationRepository.save(recommendation);
+        return repo.save(recommendation);
     }
 
     @Override
     public List<Recommendation> getAllRecommendations() {
-        return recommendationRepository.findAll();
+        return repo.findAll();
     }
 }
