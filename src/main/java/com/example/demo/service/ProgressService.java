d141094a7a1676dccbff22bff42b0c47e5816658
bala-amypo <bala@amypo.in>
2025-12-22 08:48:00 +0000

    User commit

--- src/main/java/com/example/demo/service/ProgressService.java
+++ src/main/java/com/example/demo/service/ProgressService.java
@@ -4,6 +4,9 @@ import com.example.demo.model.Progress;
 import java.util.List;
 
 public interface ProgressService {
+
     Progress create(Progress progress);
+
     List<Progress> getAll();
+    
 }
