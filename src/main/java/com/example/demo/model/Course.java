4b5cd5164f974b3ca101d46a941b78546a5e73a7
bala-amypo <bala@amypo.in>
2025-12-23 05:45:41 +0000

    User commit

--- src/main/java/com/example/demo/model/Course.java
+++ src/main/java/com/example/demo/model/Course.java
@@ -4,8 +4,10 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
+@Table(name = "courses")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -24,6 +26,13 @@ public class Course {
 
     @Column(nullable = false)
     private String category;
+
+    @ManyToOne
+    @JoinColumn(name = "instructor_id", nullable = false)
+    private User instructor;
+
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL)
+    private List<MicroLesson> lessons;
 
     private LocalDateTime createdAt;
 
