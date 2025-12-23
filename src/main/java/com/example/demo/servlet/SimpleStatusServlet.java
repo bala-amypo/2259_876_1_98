ceb8ea69fc1449cfa1850f21a6358c99d2f65afb
bala-amypo <bala@amypo.in>
2025-12-23 10:09:40 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -1,5 +1,15 @@
 package com.example.demo.servlet;
 
-public class SimpleStatusServlet{
-    
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SimpleStatusServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        resp.setStatus(HttpServletResponse.SC_OK);
+    }
 }
